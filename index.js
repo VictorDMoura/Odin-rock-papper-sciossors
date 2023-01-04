482569443d1f043d067fcbf930190cfbb82a589a
VictorDMoura <mouramvictor@gmail.com>
2023-01-03 22:09:51 -0300

    implementing one round of the game

--- index.js
+++ index.js
@@ -10,3 +10,41 @@ function getComputerChoice(){
     choice = Math.floor(choice);
     return choices[choice];
 }
+
+function playRound(playerSelection, computerSelection){
+    let player = playerSelection.toLowerCase();
+
+
+    if (player === "rock") {
+        if (computerSelection === "scissors") {
+            return "You Win! Rock beats Scissors";
+        } else if (computerSelection === "paper") {
+            return "You Lose! Paper beats Rock";
+        } else {
+            return "It's a tie!";
+        }
+    } else if (player === "paper") {
+        if (computerSelection === "scissors") {
+            return "You Lose! Scissors beats paper";
+        } else if (computerSelection === "rock") {
+            return "You Win! Paper beats Rock";
+        } else {
+            return "It's a tie!";
+        }
+    } else if (player === "scissors") {
+        if (computerSelection === "rock") {
+            return "You Lose! Rock beats Scissors";
+        } else if (computerSelection === "paper") {
+            return "You Win! Scissors beats paper";
+        } else {
+            return "It's a tie!"
+        }
+    }
+
+
+}
+
+const playerSelection = "Paper";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
