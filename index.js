6565d88c1313db2a0449a8f87cae075649ab47b4
VictorDMoura <mouramvictor@gmail.com>
2023-02-06 12:27:46 -0300

    UI start

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-alert("Welcome to the Rock, Paper and Scissors games!");
-alert("Please open the console to play the game");
-
 // Starting coding the game
-
 let choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
